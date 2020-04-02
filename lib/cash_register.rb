21c3fbebe5d4735cafd08a72ac26a12c648d298e
Doug Jones <dsjr1284@gmail.com>
2020-04-02 01:09:01 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,12 +12,12 @@ class CashRegister
 
   def add_item(item, price, quantity = 1)
     if quantity > 1
-      @total += price * quantity 
-      while quantity > 0 
-        @items << item    
+      @total += price * quantity
+      while quantity > 0
+        @items << item
         quantity -= 1
-      end 
-    elsif 
+      end
+    elsif
   end
 
   def apply_discount
