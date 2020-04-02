c2e1e1db72c6293706b23b1ef02259d93cb3a58d
Doug Jones <dsjr1284@gmail.com>
2020-04-02 00:33:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
+  requrie "pry"
+class Cash_register
 
+  attr_accessor 
+
+  def initialize
+    @total = total 
+  end 
+
+  def total 
+
+  end
+
+  def add_item 
+
+  end 
+
+  def apply_discount
+
+  end 
+
+  def item 
+
+  end 
+
+  def void_last_transaction
+
+  end 
+   
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+end 
