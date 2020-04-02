f9faa2f418185f84afaa56881404268e536e2b41
Doug Jones <dsjr1284@gmail.com>
2020-04-02 00:50:13 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,14 +2,16 @@
 class CashRegister
 
   attr_accessor :total
+  
 
   def initialize(discount = 0)
     @total = 0
     @discount = discount
+    @items =[]
   end
 
-  def add_item
-
+  def add_item(item, price, quantity = 1)
+        
   end
 
   def apply_discount
